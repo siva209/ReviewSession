cbad8b6e375e34c2326568fd3eaf851ae6c27a65
siva <54926950+siva209@users.noreply.github.com>
2021-03-25 11:08:26 +0530

    Update adder.py
    Added doc string

--- adder.py
+++ adder.py
@@ -10,16 +10,32 @@ class Adder(BaseScript):
         super(Adder, self).__init__()
         self.a = 10
         self.b = 20
+        self.d="hello"
+
 
     def define_args(self, parser):
+        """
+
+        :param parser: Define script specific command-line arguments.
+        @parser is a parser object created using the `argparse` modul
+
+        :return:None
+        """
+
         parser.add_argument('c', type=int, help='Number to add')
+        # parser.add_argument('e', type=str, help='Number to add')
 
     def run(self):
-        self.log.info("Starting run of script ...")
+        """
+          Override this method to define logic for `run` sub-command
 
+        """
+
+        self.log.info("Starting run of script ...")
         print(self.a + self.b + self.args.c)
 
-        self.log.info("Script is done")
+        # print(self.d+self.args.e)
+        # self.log.info("Script is done")
 
 
 if __name__ == '__main__':
